945c30301e472f64e12cb63ca71dd1ccb2b2288e
sarah-zhan <yizh.contact@gmail.com>
2023-02-16 19:05:41 -0800

    turn it a function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
 const args = process.argv.slice(2);
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data.name !== args) return 'We dont have record of this breed.';
-  if (error) throw error;
-  console.log('data:', data)
-  // console.log('typeof data:', typeof data)
-  console.log('first entry description:', data[0]['description'])
+  
+const breedFetcher = (args) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+    if (error) throw error;
+    const data = JSON.parse(body);
     
+    if (data.name !== args) return 'We dont have records of this breed.';
+    console.log('data:', data)
+    console.log('typeof data:', typeof data)
+    console.log('first entry description:', data[0]['description'])
+  
   })
 
 
+}
+
+breedFetcher(args)
 
